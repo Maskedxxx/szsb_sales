a47940685fce0b9e31781ca8a57edc699c55c334
kanunnikov.iv <kanunnikov.iv@ssnab.ru>
2025-03-03 10:22:52 +0000

    added args parser for app launch configuration
    the .env file is now configurated from args given at the launch of the app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 # main.py
 
+import argparse
+parser = argparse.ArgumentParser(description='Neuro-Assistant')
+parser.add_argument(
+    "--env-file",
+    type=str,
+    help="Path to .env file containing environment variables",
+    dest="env_file"
+)
+args = parser.parse_args()
+
 from dotenv import load_dotenv
-load_dotenv('.env.local', override=True)
+if args.env_file is not None:
+    load_dotenv(args.env_file, override=True)
 
 import os
 from fastapi import FastAPI
@@ -9,7 +20,7 @@ from api.endpoints import router as api_router
 
 app = FastAPI(
     title="AI-Assistant API",
-    description="API for the sales department ai-assistant.",
+    description="API for the sales department ai-assistant.", 
     version="0.1.0"
 )
 app.include_router(api_router)
@@ -20,5 +31,5 @@ if __name__ == "__main__":
         app,
         host=os.getenv("HOST"),
         port=int(os.getenv("PORT")),
-        log_level="debug"
+        log_level="info"
     )
