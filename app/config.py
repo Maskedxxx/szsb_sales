a05111f267416fe58474345e66b129e651770686
kanunnikov.iv <kanunnikov.iv@ssnab.ru>
2025-04-14 12:05:52 +0000

    made base dir relative dependent of file location

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 
 import os
 
-BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 ROUTES_PATH = os.path.join(BASE_DIR, "routes/")
 # Путь к json с роутерами/маршрутами
 ROUTING_TABLE_PATH = os.path.join(BASE_DIR, "routes/routing_table.json")
