4ee2e6b6810b762a181fff60b32c46c4922d61a6
kanunnikov.iv <kanunnikov.iv@ssnab.ru>
2025-04-22 12:17:00 +0000

    queryexpansion removed

--- app/core/services/__init__.py
+++ app/core/services/__init__.py
@@ -3,8 +3,6 @@
 from .semantic_routing_service import SemanticRoutingConfig, SemanticRoutingService
 from .key_selection_service import KeySelectionConfig, KeySelectionPromptTemplate, KeySelectionService
 from .reranking_service import RerankingConfig, RerankingPromptTemplate, RerankingService
-from .expanded_query_router_service import ExpandedQueryRouterService
-from .query_expansion_service import QueryExpansionConfig, QueryExpansionPromptTemplate, QueryExpansionService
 from .final_generation_service import FinalGenerationConfig, FinalGenerationPromptTemplate, FinalGenerationService
 
 __all__ = [
@@ -16,10 +14,6 @@ __all__ = [
     "RerankingConfig",
     "RerankingPromptTemplate",
     "RerankingService",
-    "ExpandedQueryRouterService",
-    "QueryExpansionConfig",
-    "QueryExpansionPromptTemplate",
-    "QueryExpansionService",
     "FinalGenerationConfig",
     "FinalGenerationPromptTemplate",
     "FinalGenerationService"
